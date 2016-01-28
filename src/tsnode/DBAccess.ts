119f6648c36a019a9a5fb1e93bf8c3d9477e36ad
Jonas <_14@jonashaas.de>
2016-01-28 12:55:06 +0100

    changed db & aggregated functions  according to new topic representation.

--- src/tsnode/DBAccess.ts
+++ src/tsnode/DBAccess.ts
@@ -14,12 +14,13 @@ abstract class DBEntry {
 // Entry class for Sensor values
 class SensorValueEntry extends DBEntry {
     driveNumber: number;
-    topic: number;
+    topic: String;
     value: any;
 
     //initialises the SensorValueEntry with its value
-    constructor(value:any, drivenr: number) {
+    constructor(topic: String, value:any, drivenr: number) {
         super();
+        this.topic = topic;
         this.value = value;
         this.driveNumber = drivenr;
     }
@@ -29,16 +30,15 @@ class SensorValueEntry extends DBEntry {
 enum fuel {Diesel, Unleaded, Super_Unleaded, Super_Plus_Unleaded}
 
 // Entry class for driver-specific info (dashboard-config, preferred fuel, whatever else we want to know)
-//TODO: find better representation for a DriverInfo's key than a string
 class DriverInfoEntry extends DBEntry {
     private dashboardConfig : number; // Must be adjusted to representation of dashboardconfig!
     private preferredFuel: fuel;
 
     //initialises a new DriverInfoEntry with a standard dashboardconfig and no preferred fuel
-    constructor () {
+    constructor (dbc: number, pf: fuel) {
         super();
-        this.dashboardConfig = 0; // TODO: must be adjusted to standard dashboardconfig
-        this.preferredFuel = null;
+        this.dashboardConfig = dbc; // TODO: must be adjusted to standard dashboardconfig
+        this.preferredFuel = pf;
     }
 
     //getter for the dashboardconfig
@@ -106,8 +106,8 @@ class LevelDBAccess {
     }
 
     //puts a new sensor value to the database
-    putSensorValue(topicID: number, value: any) {
-        this.db.put(LevelDBAccess.dateToKey(new Date()), JSON.stringify(new SensorValueEntry(value, this.DBInfo.currentDrive)), function(err) {
+    putSensorValue(topicID: String, value: any) {
+        this.db.put(LevelDBAccess.dateToKey(new Date()), JSON.stringify(new SensorValueEntry(topicID, value, this.DBInfo.currentDrive)), function(err) {
             if (err) console.log("Error in putting Entry:" + err);
         });
         this.incrementSize();
@@ -121,71 +121,91 @@ class LevelDBAccess {
 
     //puts a new driver's information to the database
     putDriverInfo(driver: String) {
-        this.db.put(driver, JSON.stringify(new DriverInfoEntry()));
+        this.db.put(driver, JSON.stringify(new DriverInfoEntry(0, fuel.Diesel))); //TODO: put standard values or values from msg
     }
 
-    //deletes the oldest data entries until the current size is 10% smaller than the maximum size
+    /*
+    if the current amount of entries in the db is greater than the set maximum size of the db, this function
+    deletes the oldest data entries until the current size is 10% smaller than the set maximum size.
+     */
     private deleteOnMaxCapacity() {
         if(this.DBInfo.size > this.DBInfo.maxCapacity) {
-            var listOfEntries: number[] = [];
-            this.db.createKeyStream().on('data', function(data){
-                listOfEntries.push(data);
+            var listOfKeys: any[] = [];
+            this.db.createKeyStream().on('data', function(data){ //function on every item of the stream; adds all keys to an array
+                listOfKeys.push(data);
+            }).on('end', function() { //function on the end of the stream, does the actual reducing
+                listOfKeys.sort();
+                var newSize: number = this.DBInfo.maxCapacity * 0.9;
+                var i: number = 0;
+                while(this.DBInfo.size > newSize){
+                    //if a key is a number (= belongs to a value entry) and is among the oldest keys, it is deleted:
+                    if(!isNaN(listOfKeys[i] + 0)) {
+                        this.db.del(listOfKeys[i], function (err) {
+                            if (err) console.log("Error while deleting entries: " + err);
+                        });
+                        this.decrementSize();
+                    }
+                    i++;
+                }
             });
-            listOfEntries.sort();
-            var newSize: number = this.DBInfo.maxCapacity * 0.9;
-            var i: number = 0;
-            while(this.DBInfo.size > newSize){
-                this.db.del(listOfEntries[i], function(err) {
-                    if(err) console.log("Error while deleting entries: " + err);
-                });
-                this.decrementSize();
-                i++;
-            }
         }
     }
 
     //increments the variables for the size of the database both locally and in the db entry
     private incrementSize() {
         this.DBInfo.size++;
-        this.db.put("INFO", JSON.stringify(this.DBInfo), function(err){if(err){ console.log("Error in putting updated size to the Database:" + err)}});
+        this.db.put("INFO", JSON.stringify(this.DBInfo), function(err){
+            if(err){
+                console.log("Error in putting updated size to the Database:" + err)
+            }
+        });
     }
 
     //decrements the variables for the size of the database both locally and in the db entry
     private decrementSize() {
         this.DBInfo.size--;
-        this.db.put("INFO", JSON.stringify(this.DBInfo), function(err){if(err){console.log("Error in putting updated size to the Database:" + err)}});
+        this.db.put("INFO", JSON.stringify(this.DBInfo), function(err){
+            if(err){
+                console.log("Error in putting updated size to the Database:" + err)
+            }
+        });
     }
 
-    //returns all entries of a specific topic, optionally sorted by a specific timeframe
-    public getEntries(topic: number, beginDate: number, endDate: number, callback) {
+    //gives all entries of a specific topic-id as SensorValueEntries to a callback function
+    public getEntries(topicID: String, beginDate: number, endDate: number, callback) {
         var listOfKeys: number[] = [];
-        var listOfEntries: any[] = [];
+        var listOfEntries: SensorValueEntry[] = [];
         this.db.createReadStream().on('data', function(data){
             if(!isNaN(data.key - 0)) {
                     if(data.key > beginDate && data.key < endDate) {
-                        listOfKeys[listOfKeys.length] = data.key;
-                        listOfEntries[listOfEntries.length] = JSON.parse(data.value);
+                        var sve = new SensorValueEntry(JSON.parse(data.value).topic, JSON.parse(data.value).value, JSON.parse(data.value).driveNumber);
+                        if(sve.topic == topicID) {
+                            listOfKeys[listOfKeys.length] = data.key;
+                            listOfEntries[listOfEntries.length] = sve;
+                        }
                     }
             }
-        }).on('end', function(end) {
+        }).on('end', function() {
             callback(listOfEntries, listOfKeys);
         });
     }
 
-    //calls levelup to find the config entry for a certain driver
-    public getDriverEntry(driver: String) {
-        return this.db.get(driver, function(err, value) {
+    //calls levelup to find the config entry for a certain driver and gives it to a callback function
+    public getDriverEntry(driver: String, callback) {
+        this.db.get(driver, function(err, value) {
             if(err.notFound) {
                 console.log(err + "The Driver was not yet put into the Database");
             } else if(err) {
                 console.log("Error:" + err);
+            } else {
+                var dr = new DriverInfoEntry(JSON.parse(value).dashboardConfig, JSON.parse(value).preferredfFuel);
+                callback(dr);
             }
-            return JSON.parse(value);
         })
     }
 }
 
-//the BusDevice for the Database. makes the db accessible and decodes messages to db requests
+// the BusDevice for the Database. makes the db accessible and decodes messages to db requests
 class DBBusDevice extends BusDevice {
 
     private dbAccess: LevelDBAccess;
@@ -197,49 +217,53 @@ class DBBusDevice extends BusDevice {
         this.subscribe(Topic.SETTINGS_MSG);
         this.subscribe(Topic.DBREQ_MSG);
         this.subscribeAll(Topic.VALUES);
-        //TODO: subscribe to all relevant Topics available
     }
 
     public sendValueMessage(content: SensorValueEntry[], times: number[]) {
-        this.broker.handleMessage(new ValueAnswerMessage(new Topic(31, "Values from DB"), times, content));
-        //TODO: Send Message with appropriate topic to Bus
+        this.broker.handleMessage(new ValueAnswerMessage(Topic.VALUE_ANSWER_MSG, times, content));
 }
 
     //the overriden handleMessage-function. depending on the type of the message, a different action is performed.
     public handleMessage(m: Message): void {
-        //If the given message
+        //If the given message is a DBRequestMessage, the Request is handled and a response message is sent.
         if(m instanceof DBRequestMessage) {
+            //handling of a Value Request: Values are fetched from the DB and a value response is sent
             if(m.getRequest() instanceof  DBValueRequest) {
                 var dbValueReq = <DBValueRequest> m.getRequest();
-                    this.dbAccess.getEntries(dbValueReq.getTopic().getID(), LevelDBAccess.dateToKey(dbValueReq.getBeginDate()), LevelDBAccess.dateToKey(dbValueReq.getEndDate()), function(res, tim){
+                    this.dbAccess.getEntries(dbValueReq.getTopic(), LevelDBAccess.dateToKey(dbValueReq.getBeginDate()), LevelDBAccess.dateToKey(dbValueReq.getEndDate()), function(res, tim){
                         this.sendValueMessage(res, tim);
                     });
-            } else if (m.getRequest() instanceof DBDriverInfoRequest) {
-                var n = <DBDriverInfoRequest>m.getRequest();
-                this.dbAccess.getDriverEntry(n.getDriver());
             }
-        } else if (m instanceof ValueMessage) {
-            this.dbAccess.putSensorValue(m.getTopic().getID(), m.value);
+            //handling of a Driver Info Request: the Info is fetched from the DB and a driver response is sent
+            else if (m.getRequest() instanceof DBDriverInfoRequest) {
+                var n = <DBDriverInfoRequest>m.getRequest();
+                this.dbAccess.getDriverEntry(n.getDriver(), function(dr: DriverInfoEntry){
+                    this.sendDriverMessage(dr); //TODO: actually implement driver message
+                });
+            }
+        }
+        //If the given message is a regular value message, it is written to the db
+        else if (m instanceof ValueMessage) {
+            this.dbAccess.putSensorValue(m.value.getIdentifier(), m.value.numericalValue);
       //  } else if (m instanceof SettingsMessage) {
             //TODO: understand the organisation of settingsMessages and put them to the db accordingly
         }
-        //TODO: ELSE get sensor value from message and write to db / get config from message, ...
     }
 }
 
 class DBValueRequest extends DBRequest {
-    private topic: Topic;
+    private topic: String;
     private beginDate: Date;
     private endDate: Date;
 
-    constructor (topic: Topic, beginDate: Date, endDate: Date) {
+    constructor (topic: String, beginDate: Date, endDate: Date) {
         super();
         this.topic = topic;
         this.beginDate = beginDate;
         this.endDate = endDate;
     }
 
-    public getTopic(): Topic {
+    public getTopic(): String {
         return this.topic;
     }
 
