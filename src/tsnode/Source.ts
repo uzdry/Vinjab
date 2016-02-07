13e564692bac0484ef3513c1c81c8bb65d6fd27a
SpringVaS <uidpn@student.kit.edu>
2016-02-07 20:25:59 +0100

    Dummy

--- src/tsnode/Source.ts
+++ src/tsnode/Source.ts
@@ -10,21 +10,23 @@ class Source extends BusDevice {
     value: Value;
     topic: Topic;
     cnt: number = 0;
+    interval: number;
 
 
-    constructor(pTopic: Topic) {
+    constructor(pTopic: Topic, pInterval: number) {
         super();
         this.value = new Value(0, 'km/h');
         this.cnt = 0;
         this.topic = pTopic;
+        this.interval = pInterval;
     }
 
     public fire() {
-        setInterval(this.update.bind(this),10);
+        setInterval(this.update.bind(this),50);
     }
 
     private update(): void {
-        this.value = new Value(this.cnt++ % 200, 'km/h');
+        this.value = new Value((this.cnt+=this.interval + 1) % (200 - this.interval), 'km/h');
         var m: Message = new ValueMessage(this.topic, this.value);
         this.broker.handleMessage(m);
     }
@@ -35,23 +37,12 @@ class Source extends BusDevice {
     }
 
     /*
-    te
-    */
+     te
+     */
     public publish(t: Topic): void {
         var m: Message = new ValueMessage(t, this.value);
         Broker.get().handleMessage(m);
     }
 
 }
-
-class TerminalProxy extends BusDevice {
-
-    public handleMessage(m: Message) {
-        if (m instanceof ValueMessage) {
-            console.log(m.value.numericalValue());
-        }
-    }
-
-}
-
-export {Source, TerminalProxy};
+export {Source};
