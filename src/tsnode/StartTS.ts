43d4f0d7a0e5255b573f0cf05ca3cd262fb3c8af
SpringVaS <uidpn@student.kit.edu>
2016-02-07 20:24:42 +0100

    lib

--- src/tsnode/StartTS.ts
+++ src/tsnode/StartTS.ts
@@ -1,34 +1,42 @@
-import {Source, TerminalProxy} from "./Source";
+import {Source} from "./Source";
 import {BusDevice} from "./Bus";
 import {Topic, Message} from "./messages";
 import * as DBAccess from "./DBAccess";
 import {FuelConsumption} from "./AggregatedFunctions";
 import {Proxy} from "./Proxy"
+import {Server} from "./Server";
 
-var terProx: Proxy = new Proxy();
-terProx.subscribe(Topic.SPEED);
+//var terProx: Proxy = new Proxy();
+//terProx.subscribe(Topic.SPEED);
 
-var sources: Set<BusDevice> = new Set<BusDevice>();
+var server = new Server();
 
-sources.add(new Source(Topic.SPEED));
-sources.add(new Source(Topic.MAF));
-
-var af = new FuelConsumption();
-af.init();
+var sources: Array<Source> = new Array<Source>();
 
 
-var iter = sources.values();
+for (var i = 0; i < Topic.VALUES.length; i++) {
+    var s: Source = new Source(Topic.VALUES[i], i);
+    sources.push(s);
+}
 
-var x;
-while((x = iter.next().value) != null) {
-    x.fire();
+console.log(sources);
+
+
+for (var i = 0; i < sources.length; i++) {
+    sources[i].fire();
+}
+
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+        if ((new Date().getTime() - start) > milliseconds){
+            break;
+        }
+    }
 }
 
 
 //console.log("Gokkk");
 
 
-
-
-//process.exit(1);
 
