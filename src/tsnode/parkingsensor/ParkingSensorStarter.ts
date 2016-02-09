722c24bcb29ee815b70f335fd8daf0896d1e03cd
dave339 <ugduj@student.kit.edu>
2016-02-09 14:24:39 +0100

    Parking Sensor - Messages received from the Terminal will now be displayed. Subscription to steering has been added.

--- src/tsnode/parkingsensor/ParkingSensorStarter.ts
+++ src/tsnode/parkingsensor/ParkingSensorStarter.ts
@@ -1,6 +1,4 @@
-/**
- * Created by Ray on 07.02.2016.
- */
+/// <reference path="../../../typings/postal/postal.d.ts"/>
 
 module Format {
     export class RGB {
@@ -2085,9 +2083,31 @@ module Debug {
         }
     }
 
+    var textNode = document.createElement("div");
+    textNode.id = "msgDIV";
+    textNode.innerHTML = "No messages received yet.";
+    document.getElementById("divPS").appendChild(textNode);
+
     Debug.DebugGUI.drawCommonDebugGUI();
 
     ARFactory.Drawer.drawSimpleCircleAutoClear(myisvg);
 
     myisvg.refresh();
 }
+
+class Communicator {
+    private mychannel;
+    public subscribe() : void {
+        this.mychannel = postal.channel("values");
+
+        this.mychannel.subscribe("value.steering", this.onMessageReceived.bind(this));
+    }
+
+    public onMessageReceived(data) : void {
+        var msgdiv = document.getElementById("msgDIV");
+        msgdiv.innerHTML = "Message received: " + data;
+    }
+}
+
+var com : Communicator = new Communicator();
+com.subscribe();
