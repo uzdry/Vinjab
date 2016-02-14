61fbdb560ac5886f66fc05d0fe9453c749216bd2
dave339 <ugduj@student.kit.edu>
2016-02-14 19:52:02 +0100

    Parking sensor debugger removed.

--- src/tsnode/parkingsensor/ParkingSensorStarter.ts
+++ src/tsnode/parkingsensor/ParkingSensorStarter.ts
@@ -1,5 +1,3 @@
-/// <reference path="../../../typings/postal/postal.d.ts"/>
-
 module Format {
     export class RGB {
         private red : number;
@@ -2094,20 +2092,3 @@ module Debug {
 
     myisvg.refresh();
 }
-
-class Communicator {
-    private mychannel;
-    public subscribe() : void {
-        this.mychannel = postal.channel("values");
-
-        this.mychannel.subscribe("value.steering", this.onMessageReceived.bind(this));
-    }
-
-    public onMessageReceived(data) : void {
-        var msgdiv = document.getElementById("msgDIV");
-        msgdiv.innerHTML = "Message received: " + data;
-    }
-}
-
-var com : Communicator = new Communicator();
-com.subscribe();
