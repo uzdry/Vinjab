73130820900efd0b1f06eadb17247482096e70e5
Nicolas <uzdry@student.kit.edu>
2016-02-17 14:13:54 +0100

    Standart-config gets loaded from server

--- src/tsnode/Terminal.ts
+++ src/tsnode/Terminal.ts
@@ -47,6 +47,7 @@ class Terminal {
     public incomingMsg(msg) {
         var message = JSON.parse(msg);
 
+
         //setTimeout(this.synchronousPublish.bind(this), 0, message);
         this.channelval.publish(message.topic.name, message);
 
